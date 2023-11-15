80660c9aa9c738becb527b638531b271b5dbedc7
Martina Alejandra Siscovich <135166098+MarSiscovich@users.noreply.github.com>
2023-11-14 23:34:14 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,1 +1,28 @@
 .8086
+.model small
+.stack 100h
+.data
+	titulo	db "------------------------------------------------------",0dh,0ah
+			db "|                                                    |",0dh,0ah			
+			db "|                    TRAGA BOLAS                     |",0dh,0ah
+			db "|                                                    |",0dh,0ah
+			db "------------------------------------------------------",0dh,0ah
+
+.code 
+
+extrn carga:proc
+extrn imprimir:proc
+
+main proc
+	mov ax, @data 
+	mov ds, ax
+
+	mov bx, offset titulo
+	push bx 
+	call imprimir
+
+	mov ax, 4c00h
+	int 21h
+main endp
+
+end
