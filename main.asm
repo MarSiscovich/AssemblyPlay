aefb766c0c2dda6e76b3afa9a8fa7f2baf4c7fdb
Martina Alejandra Siscovich <135166098+MarSiscovich@users.noreply.github.com>
2023-11-16 15:51:45 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -19,29 +19,30 @@ main proc
 	mov ds, ax
 
 ;---------limpiado de pantalla--------
-	; Mueve el cursor a la esquina superior izquierda
-    mov ah, 2     ; Función 02h - Posiciona el cursor
-    mov bh, 0     ; Página de video (normalmente 0)
-    mov dh, 0     ; Fila
-    mov dl, 0     ; Columna
-    int 10h       ; Llama a la interrupción 10h para posicionar el cursor
+	mov bh, 0  			; Página de video (normalmente 0)
+	push bx			
+	mov dh, 0 			; Fila
+	mov dl, 0 			; Columna
+	push dx
+	call moverCursorIzq 		; Mueve el cursor a la esquina superior izquierda
 
-    ; Llena la pantalla con espacios en blanco
-    mov ah, 9     ; Función 09h - Escribir caracter y atributo en pantalla
-    mov al, 20h   ; Carácter de espacio en blanco
-    mov bh, 0     ; Página de video (normalmente 0)
-    mov cx, 0     ; Número de veces que se repetirá la escritura (limpia toda la pantalla)
-    int 10h       ; Llama a la interrupción 10h para escribir en pantalla
+    	mov bh, 0     			; Página de video (normalmente 0)
+   	push bx
+    	mov cx, 0    		 	; Número de veces que se repetirá la escritura (limpia toda la pantalla)
+    	push cx
+    	call llenaBlanco 		; Llena la pantalla con espacios en blanco
 ;---------fin limpiado----------------
 
 ;---------prueba color-------
-	mov ax, 0600h
-	mov bh, 9 ;color de letra y fondo
-	mov ch, 0	;punto inicial hacia abajo
-	mov cl, 0	;punto inicial hacia la derecha
-	mov dh, 50	;filas 
-	mov dl, 80 	;columnas
-	int 10h
+	mov bh, 10
+	push bx
+	mov ch, 0			; Punto inicial hacia abajo
+	mov cl, 0			; Punto inicial hacia la derecha
+	push cx
+	mov dh, 50			; Filas 
+	mov dl, 80 			; Columnas
+	push dx
+	call pruebaColor
 ;---------fin prueba---------
 
 	mov bx, offset titulo
