f8260576008ac830d7adcd012ca50360ce4eacb4
castiel21 <149549087+castiel21@users.noreply.github.com>
2023-11-24 01:20:37 -0300

    Update main.asm
     agregado intro 1 y 2

--- main.asm
+++ main.asm
@@ -5,79 +5,154 @@
 	;Datos del menu y resto
 	;Graficos
 		   
+		inicio1 db" __   __   __   __   ______   __   ______  __   __  _____    ______",0dh,0ah   
+        db"/\ \ /\  -.\ \ /\ \ /\  ___\ /\ \ /\  __ \/\  -.\ \/\  __-. /\  __ \",0dh,0ah   
+        db"\ \ \\ \ \-.  \\ \ \\ \ \____\ \ \\ \  __ \ \ \-.  \ \ \/\ \\ \ \/\ \",0dh,0ah  
+        db" \ \_\\ \_\\ \_\\ \_\\ \_____\\ \_\\ \_\ \_\ \_\\ \_\ \____- \ \_____\",0dh,0ah 
+        db"  \/_/ \/_/ \/_/ \/_/ \/_____/ \/_/ \/_/\/_/\/_/ \/_/\/____/  \/_____/ ",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db" __    __   __   ______   ______  ______   ______   ______   ______  ______  ",0dh,0ah
+        db"/\  -./  \ /\ \ /\  ___\ /\  == \/\  __ \ /\  ___\ /\  __ \ /\  ___\/\__  _\ ",0dh,0ah
+        db"\ \ \-./\ \\ \ \\ \ \____\ \  __<\ \ \/\ \\ \___  \\ \ \/\ \\ \  __\\/_/\ \/ ",0dh,0ah
+        db" \ \_\ \ \_\\ \_\\ \_____\\ \_\ \_\ \_____\\/\_____\\ \_____\\ \_\     \ \_\ ",0dh,0ah
+        db"  \/_/  \/_/ \/_/ \/_____/ \/_/ /_/\/_____/ \/_____/ \/_____/ \/_/      \/_/ ",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"TOCAME UN NUMERITO PORFAVOR MAQUINA DEL MAL",0dh,0ah,24h    
+ 
+inicio2 db"                                              _           _        ",0dh,0ah
+        db"     ___   _ __  __ _   _   _  _ __   __ _   (_) ___   __| |  __ _ ",0dh,0ah
+        db"    / _ \ | '__|/ _` | | | | ||  _ \ / _` |  | |/ _ \ / _` | / _` |",0dh,0ah
+        db"   |  __/ | |  | (_| | | |_| || | | | (_| |  | | (_) | (_| || (_| |",0dh,0ah
+        db"    \___| |_|   \__,_|  \__,_||_| |_|\__,_| _/ |\___/ \__,_| \__,_|",0dh,0ah
+        db"                                           |__/                    ",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah        
+        db"",0dh,0ah
+        db"",0dh,0ah
+       db"         ESPERO QUE SE VUELVAN EXPERTOS EN.....",0dh,0ah,0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah
+        db"",0dh,0ah        
+        db"TOCAME OTRO NUMERITO SE QUE TE QUEDASTE CON GANAS",0dh,0ah,24h         
+
+
 		titulo		db "",0dh,0ah
-				db "",0dh,0ah
-				db "	  ______                             ____        __          ",0dh,0ah
-				db "	 /_  __/________ _____ _____ _      / __ )____  / /___ ______",0dh,0ah			
-				db "	  / / / ___/ __ `/ __ `/ __ `/_____/ __  / __ \/ / __ `/ ___/",0dh,0ah
-				db "	 / / / /  / /_/ / /_/ / /_/ /_____/ /_/ / /_/ / / /_/ (__  ) ",0dh,0ah
-				db "	/_/ /_/   \__,_/\__, /\__,_/     /_____/\____/_/\__,_/____/  ",0dh,0ah
-				db "	               /____/                                        ",0dh,0ah
-				db "",0dh,0ah,24h
+					db "",0dh,0ah
+					db "	  ______                             ____        __          ",0dh,0ah
+					db "	 /_  __/________ _____ _____ _      / __ )____  / /___ ______",0dh,0ah			
+					db "	  / / / ___/ __ `/ __ `/ __ `/_____/ __  / __ \/ / __ `/ ___/",0dh,0ah
+					db "	 / / / /  / /_/ / /_/ / /_/ /_____/ /_/ / /_/ / / /_/ (__  ) ",0dh,0ah
+					db "	/_/ /_/   \__,_/\__, /\__,_/     /_____/\____/_/\__,_/____/  ",0dh,0ah
+					db "	               /____/                                        ",0dh,0ah,0dh,0ah,24h
 					
 		subt		db "              COCODRILO QUE DUERME ES CARTERA",0dh,0ah,0dh,0ah,24h
 
 		menu		db "",0dh,0ah
-				db "",0dh,0ah
-				db "                             PULSE 1 PARA JUGAR",0dh,0ah
-				db "",0dh,0ah
-				db "                         PULSE 2 PARA INSTRUCCIONES ",0dh,0ah
-				db "",0dh,0ah
-				db "                            PULSE 3 PARA CREDITOS",0dh,0ah
-				db "",0dh,0ah
-				db "                         PULSE 4 PARA SALIR DEL JUEGO",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah,24h
+					db "",0dh,0ah
+					db "                             PULSE 1 PARA JUGAR",0dh,0ah
+					db "",0dh,0ah
+					db "                         PULSE 2 PARA INSTRUCCIONES ",0dh,0ah
+					db "",0dh,0ah
+					db "                            PULSE 3 PARA CREDITOS",0dh,0ah
+					db "",0dh,0ah
+					db "                         PULSE 4 PARA SALIR DEL JUEGO",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah,24h
 
 		instruct		db "",0dh,0ah
-				db "   ___               _                            _                          ",0dh,0ah
-				db "  |_ _|  _ _    ___ | |_   _ _   _  _   __   __  (_)  ___   _ _    ___   ___ ",0dh,0ah
-				db "   | |  | ' \  (_-< |  _| | '_| | || | / _| / _| | | / _ \ | ' \  / -_) (_-< ",0dh,0ah
-				db "  |___| |_||_| /__/  \__| |_|    \_,_| \__| \__| |_| \___/ |_||_| \___| /__/ ",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "      - EL COCODRILO SE MUEVE CON LAS TECLAS: ('A','D')",0dh,0ah
-				db "      - El OBJETIVO ES: Comerme la mayor cantidad de bolas frescas y limpias ",0dh,0ah
-				db "      - Vas a perder una vida si se te cae una bola.",0dh,0ah
-				db "      - Si perdes los 5 corazones fuiste, sos cartera lacoste",0dh,0ah
-				db "      - En el juego presione 'P' para volver al menu de inicio.",0dh,0ah
-				db "      - Otra instruccion xd",0dh,0ah
-				db "      - Otra instruccion xd",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "!INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU! ",0dh,0ah,24h
+					db "   ___               _                            _                          ",0dh,0ah
+					db "  |_ _|  _ _    ___ | |_   _ _   _  _   __   __  (_)  ___   _ _    ___   ___ ",0dh,0ah
+					db "   | |  | ' \  (_-< |  _| | '_| | || | / _| / _| | | / _ \ | ' \  / -_) (_-< ",0dh,0ah
+					db "  |___| |_||_| /__/  \__| |_|    \_,_| \__| \__| |_| \___/ |_||_| \___| /__/ ",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "      - EL COCODRILO SE MUEVE CON LAS TECLAS: ('A','D')",0dh,0ah
+					db "      - El OBJETIVO ES: Comer la mayor cantidad de bolainas ",0dh,0ah
+					db "      - Vas a perder una vida si se te cae una suculenta.",0dh,0ah
+					db "      - Si perdes los 5 corazones fuiste, sos cartera lacoste",0dh,0ah
+					db "      - En el juego presione 'P' para volver al menu de inicio.",0dh,0ah
+					db "      - Otra instruccion xd",0dh,0ah
+					db "      - Otra instruccion xd",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "!INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU! ",0dh,0ah,24h
 
 		creditos	db"                            _   _   _                      ",0dh,0ah               
-				db"    ___   _ __    ___    __| | (_) | |_    ___    ___      ",0dh,0ah
-				db"   / __| | '__|  / _ \  / _` | | | | __|  / _ \  / __|  o  ",0dh,0ah
-				db"  | (__  | |    |  __/ | (_| | | | | |_  | (_) | \__ \  o  ",0dh,0ah
-				db"   \___| |_|     \___|  \__,_| |_|  \__|  \___/  |___/     ",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "      -Martina (la jefa) Siscovich",0dh,0ah
-				db "      -Santiago (el amante de microsoft) Rodriguez ",0dh,0ah
-				db "      -Lorenzo (el proplayer de osu) Graizzaro ",0dh,0ah
-				db "      -Damian (el alcoholico) Cabral",0dh,0ah
-				db "      -Agustin (el esclavo del mac) Lopez ",0dh,0ah
-				db "      -julian (el desamparado) barberis",0dh,0ah 
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "",0dh,0ah
-				db "!INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU! ",0dh,0ah,24h
+					db"    ___   _ __    ___    __| | (_) | |_    ___    ___      ",0dh,0ah
+					db"   / __| | '__|  / _ \  / _` | | | | __|  / _ \  / __|  o  ",0dh,0ah
+					db"  | (__  | |    |  __/ | (_| | | | | |_  | (_) | \__ \  o  ",0dh,0ah
+					db"   \___| |_|     \___|  \__,_| |_|  \__|  \___/  |___/     ",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "      -Martina (la jefa) Siscovich",0dh,0ah
+					db "      -Santiago (el amante de microsoft) Rodriguez ",0dh,0ah
+					db "      -Lorenzo (el proplayer de osu) Graizzaro ",0dh,0ah
+					db "      -Damian (el alcoholico) Cabral",0dh,0ah
+					db "      -Agustin (el esclavo del mac) Lopez ",0dh,0ah
+					db "      -julian (el desamparado) barberis",0dh,0ah 
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "",0dh,0ah
+					db "!INGRESE CUALQUIER NUMERO PARA VOLVER AL MENU! ",0dh,0ah,24h
+
+		coraVacio   db "     ____         ____               ", 0dh, 0ah
+					db "   _|____|_     _|____|_            ", 0dh, 0ah
+					db " _|_|    |_| _ |_|    |_|_          ", 0dh, 0ah
+					db "|_|         |_|         |_|         ", 0dh, 0ah
+					db "|_|                     |_|         ", 0dh, 0ah
+					db "|_|_                   _|_|         ", 0dh, 0ah
+					db " |_|_                 _|_|           ", 0dh, 0ah
+					db "   |_|_             _|_|             ", 0dh, 0ah
+					db "     |_|_         _|_|               ", 0dh, 0ah
+					db "       |_|_     _|_|                 ", 0dh, 0ah
+					db "          |_|_|_|                     ", 0dh, 0ah
+					db "            |_|                       ", 0dh, 0ah, 24h		
+
+		cocodrilo   db "					     _ _ _           _ _ _				",0dh,0ah	
+					db "					   _|_|_|_|_	    |_|_|_|_			",0dh,0ah	
+					db "		       			 _|_|	  |_| _	  _|_|    |_|_			",0dh,0ah	
+					db "	     _ _ _	   		|_|	_    |_|_|_|        |_|       ",0dh,0ah	
+					db "	   _|_|_|_|_  		   	|_|    |_|   |_|_|_|  |_|   |_|			",0dh,0ah	
+					db "	 _|_|	  |_|_ _ _ _ _ _ _ _ _ _|_|    |_|	      |_|   |_|			",0dh,0ah	
+					db "  	|_|	    |_|_|_|_|_|_|_|_|_|_|_|    |_|            |_|   |_|  	",0dh,0ah	
+					db " 	|_|						            |_|			",0dh,0ah	
+					db " 	|_|					      		    |_|			",0dh,0ah	
+					db " 	|_|       _   _   _   _	  _   _			      	    |_|			",0dh,0ah	
+					db " 	|_|_    _|_|_|_|_|_|_|_|_|_|_|_|_ 			    |_|			",0dh,0ah	
+					db " 	  |_|_ |_| |_| |_| |_| |_| |_| |_|			    |_|			",0dh,0ah	
+					db "  	    |_|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|_|			",0dh,0ah	
+					db "          |_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|			",0dh,0ah,24h
 	;fingraficos
 		opcion		db "x"
 		tiempo_aux  DB 0
@@ -105,7 +180,84 @@
 	main proc
 		mov ax, @data 
 		mov ds, ax
+;---------limpiado de pantalla--------
+			; Mueve el cursor a la esquina superior izquierda
 
+			mov bh, 0     ; Página de video (normalmente 0)
+			push bx
+			mov dh, 0     ; Fila
+			mov dl, 0     ; Columna
+			push dx
+			call moverCursorIzq
+
+			mov ah, 0fh
+			int 10h
+			mov ah, 0
+			int 10h			;LIMPIEZA DE PANTALLA CONVERTIR EN LIBRERIA
+		;---------fin limpiado----------------
+
+		;---------prueba color-------
+			mov bh, 10
+			push bx
+			mov ch, 0							; Punto inicial hacia abajo
+			mov cl, 0							; Punto inicial hacia la derecha
+			push cx
+			mov dh, 50							; Filas 
+			mov dl, 80 							; Columnas
+			push dx
+			call pruebaColor
+			;---------fin prueba---------
+
+		;---------CARTELES DE INICIO------
+         mov bx,offset inicio1
+        push bx
+        call imprimir
+       mov bx, offset opcion
+				push bx
+				mov dl, 1
+				push dx
+				mov ah, 1
+				push ax
+				call carga
+				;---------limpiado de pantalla--------
+			; Mueve el cursor a la esquina superior izquierda
+
+			mov bh, 0     ; Página de video (normalmente 0)
+			push bx
+			mov dh, 0     ; Fila
+			mov dl, 0     ; Columna
+			push dx
+			call moverCursorIzq
+
+			mov ah, 0fh
+			int 10h
+			mov ah, 0
+			int 10h			;LIMPIEZA DE PANTALLA CONVERTIR EN LIBRERIA
+		;---------fin limpiado----------------
+
+		;---------prueba color-------
+			mov bh, 10
+			push bx
+			mov ch, 0							; Punto inicial hacia abajo
+			mov cl, 0							; Punto inicial hacia la derecha
+			push cx
+			mov dh, 50							; Filas 
+			mov dl, 80 							; Columnas
+			push dx
+			call pruebaColor
+;---------fin prueba---------
+
+			mov bx,offset inicio2
+			push bx
+			call imprimir
+
+			 mov bx, offset opcion
+				push bx
+				mov dl, 1
+				push dx
+				mov ah, 1
+				push ax
+				call carga
 		comienzo:
 		;---------limpiado de pantalla--------
 			; Mueve el cursor a la esquina superior izquierda
